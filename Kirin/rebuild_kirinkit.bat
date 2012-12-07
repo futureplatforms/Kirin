4f92a6b1ed2854bf8b182bb3a1427f1189b7fc06
dhoskins <douglas.hoskins@futureplatforms.com>
2012-12-07 11:09:54 +0000

    tweaking scriptoids

--- Kirin/rebuild_kirinkit.bat
+++ Kirin/rebuild_kirinkit.bat
@@ -1,5 +1,3 @@
-set KIRIN_HOME=c:\workspace\vexed\KirinJS\Kirin
-
 REM build the windows stuffs
-c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild %KIRIN_HOME%\core\wp8\wp8\KirinWP8\KirinWP8.csproj
-c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild %KIRIN_HOME%\core\w8\w8\KirinW8\KirinW8.csproj
+c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild core\wp8\wp8\KirinWP8\KirinWP8.csproj
+c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild core\w8\w8\KirinW8\KirinW8.csproj
