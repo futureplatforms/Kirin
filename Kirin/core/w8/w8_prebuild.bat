7d48fe5767ad669162b2523b9027c9070a863e0b
dhoskins <douglas.hoskins@futureplatforms.com>
2013-01-04 09:31:30 +0000

    fixing w8 stuffs

--- Kirin/core/w8/w8_prebuild.bat
+++ Kirin/core/w8/w8_prebuild.bat
@@ -1,3 +1,4 @@
 set KIRIN_HOME=c:\workspace\vexed\KirinJS\Kirin
-set NODE=c:\Program Files\nodejs\node 
-NODE build.js --platform w8 --verbose --minify
+set NODE="c:\Program Files\nodejs\node"
+cd w8\KirinW8Test
+NODE ..\..\build.js --platform w8 --verbose --minify
