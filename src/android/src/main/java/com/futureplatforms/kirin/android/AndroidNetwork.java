17b0f56aaf8b3e8931d79aa87165997017271449
Kassim <kassim@futureplatforms.com>
2013-08-06 17:17:12 +0100

    Android Update (before adding ActionBar)

--- src/android/src/main/java/com/futureplatforms/kirin/android/AndroidNetwork.java
+++ src/android/src/main/java/com/futureplatforms/kirin/android/AndroidNetwork.java
@@ -18,181 +18,162 @@ import android.os.AsyncTask;
 
 import com.futureplatforms.kirin.dependencies.StaticDependencies.NetworkDelegate;
 
-public class AndroidNetwork extends NetworkDelegate
-{
-	private class GetAsyncTask extends AsyncTask<Object, Void, Boolean>
-	{
+public class AndroidNetwork extends NetworkDelegate {
+	private class GetAsyncTask extends AsyncTask<Object, Void, Boolean> {
 		String url;
 		Map<String, String> headers;
 		NetworkResponse callback;
-		
+
 		int res = 200;
 		String result;
 		Map<String, String> responseHeaderMap;
-		
+
 		String code;
-		
+
 		@SuppressWarnings("unchecked")
 		@Override
-		protected Boolean doInBackground(Object... params)
-		{
+		protected Boolean doInBackground(Object... params) {
 			url = (String) params[0];
 			headers = (Map<String, String>) params[1];
 			callback = (NetworkResponse) params[2];
-			
-			try
-			{
-				
+
+			try {
+
 				HttpClient client = new DefaultHttpClient();
-				
+
 				HttpGet get = new HttpGet(url);
-				
-				for (String name : headers.keySet())
-				{
-					get.setHeader(name, headers.get(name));
-				}
-				
+
+				if (headers != null)
+					for (String name : headers.keySet()) {
+						get.setHeader(name, headers.get(name));
+					}
+
 				HttpResponse getResponse = null;
-				
+
 				getResponse = client.execute(get);
-				
+
 				Header[] responseHeaders = getResponse.getAllHeaders();
-				
+
 				responseHeaderMap = new HashMap<String, String>();
-				for (Header h : responseHeaders)
-				{
+				for (Header h : responseHeaders) {
 					responseHeaderMap.put(h.getName(), h.getValue());
 				}
-				
+
 				result = EntityUtils.toString(getResponse.getEntity());
 				return true;
-				
-			}
-			catch (Exception e)
-			{
+
+			} catch (Exception e) {
 				code = e.getLocalizedMessage();
 				return false;
 			}
-			
+
 		}
-		
+
 		@Override
-		protected void onPostExecute(Boolean success)
-		{
+		protected void onPostExecute(Boolean success) {
 			super.onPostExecute(success);
-			
-			if (success)
-			{
+
+			if (success) {
 				callback.onSuccess(res, result, responseHeaderMap);
-			}
-			else
-			{
+			} else {
 				callback.onFail(code);
 			}
 		}
-		
+
 	}
-	
-	private class PostPutAsyncTask extends AsyncTask<Object, Void, Boolean>
-	{
+
+	private class PostPutAsyncTask extends AsyncTask<Object, Void, Boolean> {
 		String url;
 		String toPost;
 		Map<String, String> headers;
 		NetworkResponse callback;
-		
+
 		int res = 200;
 		String result;
 		Map<String, String> responseHeaderMap;
-		
+
 		String code;
 		private HttpVerb _Verb;
+
 		public PostPutAsyncTask(HttpVerb verb) {
-		    this._Verb = verb;
+			this._Verb = verb;
 		}
-		
+
 		@SuppressWarnings("unchecked")
 		@Override
-		protected Boolean doInBackground(Object... params)
-		{
+		protected Boolean doInBackground(Object... params) {
 			url = (String) params[0];
 			toPost = (String) params[1];
 			headers = (Map<String, String>) params[2];
 			callback = (NetworkResponse) params[3];
-			
-			try
-			{
+
+			try {
 				HttpClient client = new DefaultHttpClient();
-				
+
 				HttpEntityEnclosingRequestBase base;
 				if (_Verb == HttpVerb.POST) {
-				    base = new HttpPost(url);
+					base = new HttpPost(url);
 				} else if (_Verb == HttpVerb.PUT) {
-				    base = new HttpPut(url);
+					base = new HttpPut(url);
 				} else {
-				    throw new IllegalStateException("verb is " + _Verb + ", can only be put or post");
+					throw new IllegalStateException("verb is " + _Verb
+							+ ", can only be put or post");
 				}
-				for (String name : headers.keySet())
-				{
-				    base.setHeader(name, headers.get(name));
-				}
-				
+				if (headers != null)
+					for (String name : headers.keySet()) {
+						base.setHeader(name, headers.get(name));
+					}
+
 				base.setEntity(new StringEntity(toPost));
-				
+
 				HttpResponse postResponse = null;
-				
+
 				postResponse = client.execute(base);
-				
+
 				Header[] responseHeaders = postResponse.getAllHeaders();
-				
+
 				responseHeaderMap = new HashMap<String, String>();
-				for (Header h : responseHeaders)
-				{
+				for (Header h : responseHeaders) {
 					responseHeaderMap.put(h.getName(), h.getValue());
 				}
-				
+
 				result = EntityUtils.toString(postResponse.getEntity());
 				return true;
-				
-			}
-			catch (Exception e)
-			{
+
+			} catch (Exception e) {
 				code = e.getLocalizedMessage();
 				return false;
 			}
-			
+
 		}
-		
+
 		@Override
-		protected void onPostExecute(Boolean success)
-		{
+		protected void onPostExecute(Boolean success) {
 			super.onPostExecute(success);
-			
-			if (success)
-			{
+
+			if (success) {
 				callback.onSuccess(res, result, responseHeaderMap);
-			}
-			else
-			{
+			} else {
 				callback.onFail(code);
 			}
 		}
-		
+
 	}
 
-    @Override
-    public void doHttp(HttpVerb verb, String url, String payload,
-            Map<String, String> headers, NetworkResponse callback) {
-        switch (verb) {
-            case GET:
-                new GetAsyncTask().execute(url, headers, callback);
-                break;
-            case POST:
-            case PUT:
-                new PostPutAsyncTask(verb).execute(url, payload, headers, callback);
-                break;
-                
-            default:
-                throw new IllegalArgumentException("");
-        }
-    }
+	@Override
+	public void doHttp(HttpVerb verb, String url, String payload,
+			Map<String, String> headers, NetworkResponse callback) {
+		switch (verb) {
+		case GET:
+			new GetAsyncTask().execute(url, headers, callback);
+			break;
+		case POST:
+		case PUT:
+			new PostPutAsyncTask(verb).execute(url, payload, headers, callback);
+			break;
+
+		default:
+			throw new IllegalArgumentException("");
+		}
+	}
 }
