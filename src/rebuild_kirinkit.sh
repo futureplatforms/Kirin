6bdb55bf6a536cdcb32a7ee74392d7c32a5b7a73
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2013-08-28 16:13:23 +0100

    fix rebuild_kirinkit for iOS

--- src/rebuild_kirinkit.sh
+++ src/rebuild_kirinkit.sh
@@ -1,3 +1,3 @@
-cd "src/ios/KirinKit"
+cd "ios/KirinKit"
 xcodebuild -sdk iphoneos -project KirinKit.xcodeproj -target KirinKit -configuration Debug -verbose clean build
 xcodebuild -sdk iphoneos -project KirinKit.xcodeproj -target KirinKit -configuration Release -verbose clean build
