b0cd5a1b80f948dfc8f5e2cd01696f415e2e48b3
dhoskins <douglas.hoskins@futureplatforms.com>
2013-08-28 07:51:54 +0100

    Fixing windows build script paths

--- src/rebuild_kirinkit.bat
+++ src/rebuild_kirinkit.bat
@@ -1,3 +1,3 @@
 REM build the windows stuffs
-c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild core\wp8\wp8\KirinWP8\KirinWP8.csproj
-c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild core\w8\w8\KirinW8\KirinW8.csproj
+c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild wp8\wp8\KirinWP8\KirinWP8.csproj /t:Clean,Build
+c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild w8\w8\KirinW8\KirinW8.csproj /t:Clean,Build
