5eacfebb754f05366aca3230be938f0a39336fa2
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2013-08-28 16:08:00 +0100

    fix rebuild_kirinkit for iOS

--- src/rebuild_kirinkit.sh
+++ src/rebuild_kirinkit.sh
@@ -1,14 +1,2 @@
-if [[ "$KIRIN_HOME" = "" ]] ; then 
-	export KIRIN_HOME=`dirname $0`
-fi
-if [[ "$NODE" = "" ]] ; then 
-	export NODE=`which node`
-fi
-configuration=$1
-if [[ "$configuration" = "" ]] ; then 
-	configuration="Debug Release"
-fi
-rm -Rf $KIRIN_HOME/core/ios/KirinKit/build 2>/dev/null
-for conf in $configuration ; do 
-	$NODE $KIRIN_HOME/build.js --ios --ios-configuration $conf --no-js-build --tests none
-done
+xcodebuild -sdk iphoneos6.1 -project KirinKit.xcodeproj -target KirinKit -configuration Debug -verbose clean build
+xcodebuild -sdk iphoneos6.1 -project KirinKit.xcodeproj -target KirinKit -configuration Release -verbose clean build
