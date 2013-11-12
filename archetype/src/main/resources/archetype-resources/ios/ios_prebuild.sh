01c9cd71ccc835bec0b275a0ee718eae4d9c931a
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2013-11-12 09:17:18 +0000

    adding $M2 variable to maven path

--- archetype/src/main/resources/archetype-resources/ios/ios_prebuild.sh
+++ archetype/src/main/resources/archetype-resources/ios/ios_prebuild.sh
@@ -3,4 +3,4 @@ cd ..
 # -pl common/gwt ensures that only the GWT project is build (and not the Android project), 
 # and `-am` ensures that all dependencies are compiled first, i.e. the app core project.
 
-mvn -pl common/gwt -DgwtModule=${package}.${rootArtifactId}_safari_${CONFIGURATION} -am clean install
+${M2}/mvn -pl common/gwt -DgwtModule=${package}.${rootArtifactId}_safari_${CONFIGURATION} -am clean install
