e4f3092cc5d4cb7e93496285124f68267bc7aa8b
Kassim Maguire <kassim@futureplatforms.com>
2014-03-27 11:19:09 +0000

    KirinFragmentActivity

---
+++ src/android/src/main/java/com/futureplatforms/kirin/android/app/KirinFragmentActivity.java
@@ -0,0 +1,35 @@
+package com.futureplatforms.kirin.android.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.futureplatforms.kirin.IKirinNativeObject;
+import com.futureplatforms.kirin.KirinModule;
+
+abstract public class KirinFragmentActivity<Module extends KirinModule<KirinNativeObj>, KirinNativeObj extends IKirinNativeObject>
+		extends FragmentActivity implements IKirinNativeObject, IKirinModuleHost<Module> {
+	private Module module;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		KirinAndroidMethods.onCreate(this);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		KirinAndroidMethods.onUnload(this);
+	}
+
+	@Override
+	public Module getModule() {
+		return module;
+	}
+
+	@Override
+	public void setModule(Module module) {
+		this.module = module;
+	}
+
+}
