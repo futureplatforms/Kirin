9c7454a72291f9846f83a5e7ca5b3d2aa0460e9f
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2014-05-01 18:27:51 +0100

    implementing new KirinLoc for console

--- src/console/src/main/java/com/futureplatforms/kirin/console/ConsoleLocation.java
+++ src/console/src/main/java/com/futureplatforms/kirin/console/ConsoleLocation.java
@@ -1,48 +1,32 @@
 package com.futureplatforms.kirin.console;
 
+import com.futureplatforms.kirin.dependencies.AsyncCallback.AsyncCallback1;
 import com.futureplatforms.kirin.dependencies.LocationDelegate;
  
 public class ConsoleLocation implements LocationDelegate
 {
 	
-	public ConsoleLocation()
-	{
+	public ConsoleLocation() {
 	}
 
-    public void startUpdatingLocation(Accuracy accuracy, LocationCallback callback) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void startUpdatingLocation(Accuracy accuracy, long interval,
+			LocationCallback callback) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    public void getLocationContinuous(Accuracy accuracy, int intervalMs,
-            LocationCallback callback) {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void stopUpdating() {
+		// TODO Auto-generated method stub
+		
+	}
 
-    public void stopListening() {
-        // TODO Auto-generated method stub
-        
-    }
+	@Override
+	public void hasPermission(AsyncCallback1<Boolean> cb) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    public boolean getIsListening() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    public double getLatitude() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public double getLongitude() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public float getAccuracy() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
 	
 }
