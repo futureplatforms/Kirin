1cbfe73f5875f74db757aac4133de19c45afc5db
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2014-11-24 08:37:43 +0000

    [iOS]  Successfully zipping the debug and release frameworks

--- src/ios/scripts/rebuild_kirinkit.sh
+++ src/ios/scripts/rebuild_kirinkit.sh
@@ -1,2 +1,3 @@
 cd "KirinKit"
+xcodebuild -sdk iphoneos -project KirinKit.xcodeproj -target KirinKit -configuration Debug   -verbose clean build
 xcodebuild -sdk iphoneos -project KirinKit.xcodeproj -target KirinKit -configuration Release -verbose clean build
--- src/ios/scripts/zip_framework.sh
+++ src/ios/scripts/zip_framework.sh
@@ -1,5 +1,28 @@
+# Make a target folder in the root
 rm -rf target
 mkdir target
-cd KirinKit/build/Release-iphoneos
-zip --symlinks -r $1-$2.zip KirinKit.framework
-mv $1-$2.zip ../../../target
+
+# This is where the builds are
+cd KirinKit/build
+
+# Make the folder that we're going to zip
+mkdir artifacts
+mkdir artifacts/Debug
+mkdir artifacts/Release
+
+# Move the debug and release frameworks into the corresponding folders in artifacts dir
+cd Release-iphoneos
+cp -av KirinKit.framework ../artifacts/Release
+cd ../Debug-iphoneos
+cp -av KirinKit.framework ../artifacts/Debug
+
+# Now make it into a jar
+cd ../artifacts
+zip --symlinks -r $1-$2.jar Debug Release
+
+# Move it to the target folder
+mv $1-$2.jar ../../../target
+
+# And clean up
+cd ..
+rm -rf artifacts
