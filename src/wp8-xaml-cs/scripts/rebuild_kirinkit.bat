5d9a9b5a879259443c4ceba254db3173383975c8
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2014-12-15 17:11:29 +0000

    [WP8]  Fix up renames and the build script

--- src/wp8-xaml-cs/scripts/rebuild_kirinkit.bat
+++ src/wp8-xaml-cs/scripts/rebuild_kirinkit.bat
@@ -1,5 +1,5 @@
 :: build the windows stuffs
-cd wp8
+cd wp8-xaml-cs
 call wp8_prebuild.bat Release
 
 c:\windows\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:Clean,Build /property:Platform=ARM KirinWP8\KirinWP8.csproj 
