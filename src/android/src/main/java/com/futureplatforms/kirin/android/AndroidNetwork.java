16b519c53d9f9d322606d1890ae61122271b5ee0
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2016-03-09 16:01:43 +0000

    [android]  Don't use org.apache.http

--- src/android/src/main/java/com/futureplatforms/kirin/android/AndroidNetwork.java
+++ src/android/src/main/java/com/futureplatforms/kirin/android/AndroidNetwork.java
@@ -1,31 +1,27 @@
 package com.futureplatforms.kirin.android;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.futureplatforms.kirin.android.dropbox.AndroidDropboxes;
 import com.futureplatforms.kirin.dependencies.StaticDependencies;
-
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
 
 import android.os.AsyncTask;
 import android.os.Build;
 
 import com.futureplatforms.kirin.dependencies.StaticDependencies.NetworkDelegate.HttpVerb;
 import com.futureplatforms.kirin.dependencies.StaticDependencies.NetworkDelegate.NetworkResponse;
-import com.futureplatforms.kirin.dependencies.StaticDependencies.NetworkDelegate.NetworkResponse.OnCancelledListener;
 import com.futureplatforms.kirin.dependencies.StaticDependencies.NetworkDelegateClient;
 import com.google.common.io.BaseEncoding;
 
@@ -42,65 +38,119 @@ public class AndroidNetwork implements NetworkDelegateClient {
 		Plain, Base64, Token
 	}
 
-	private static String getResult(HttpReturnType returnType, HttpEntity entity) throws IOException {
+	private static String getResult(HttpReturnType returnType, HttpURLConnection conn) throws IOException {
+		InputStream is = conn.getInputStream();
 		if (returnType == HttpReturnType.Plain) {
-			return EntityUtils.toString(entity, "UTF-8");
+			return slurpString(is, 1024);
 		} else if (returnType == HttpReturnType.Base64){
-			return BaseEncoding.base64().encode(EntityUtils.toByteArray(entity));
+			return BaseEncoding.base64().encode(slurpBytes(is, 1024));
 		} else {
-			return AndroidDropboxes.getInstance()._NetworkDropbox.putItem(EntityUtils.toByteArray(entity));
+			return AndroidDropboxes.getInstance()._NetworkDropbox.putItem(slurpBytes(is, 1024));
 		}
 	}
 
-	private class GetAsyncTask extends AsyncTask<Object, Void, Boolean> {
-		String url;
-		Map<String, String> headers;
-		NetworkResponse callback;
+	private static byte[] slurpBytes(final InputStream is, final int bufferSize)
+	{
+		final byte[] buffer = new byte[bufferSize];
+		final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			try {
+				for (;;) {
+					int rsz = is.read(buffer, 0, buffer.length);
+					if (rsz < 0)
+						break;
+					out.write(buffer, 0, rsz);
+				}
+			}
+			finally {
+				is.close();
+			}
+		}
+		catch (UnsupportedEncodingException ex) {
+	    /* ... */
+		}
+		catch (IOException ex) {
+	      /* ... */
+		}
+		return out.toByteArray();
+	}
 
-		int res = 200;
-		String result;
-		Map<String, String> responseHeaderMap;
+	private static String slurpString(final InputStream is, final int bufferSize)
+	{
+		final char[] buffer = new char[bufferSize];
+		final StringBuilder out = new StringBuilder();
+		try {
+			final Reader in = new InputStreamReader(is, "UTF-8");
+			try {
+				for (;;) {
+					int rsz = in.read(buffer, 0, buffer.length);
+					if (rsz < 0)
+						break;
+					out.append(buffer, 0, rsz);
+				}
+			}
+			finally {
+				in.close();
+			}
+		}
+		catch (UnsupportedEncodingException ex) {
+	    /* ... */
+		}
+		catch (IOException ex) {
+	      /* ... */
+		}
+		return out.toString();
+	}
 
-		String code;
+	private class NetAsyncTask extends AsyncTask<Object, Void, Boolean> {
+		private String url;
+		private Map<String, String> headers;
+		private NetworkResponse callback;
+
+		private int res = 200;
+		private String result, payload;
+		private Map<String, String> responseHeaderMap;
+
+		private String code;
 		private HttpReturnType _ReturnType;
+		private final HttpVerb verb;
+
+		private NetAsyncTask(HttpVerb verb) {
+			this.verb = verb;
+		}
 		
 		@SuppressWarnings("unchecked")
 		@Override
 		protected Boolean doInBackground(Object... params) {
 			url = (String) params[0];
-			headers = (Map<String, String>) params[1];
-			callback = (NetworkResponse) params[2];
-			_ReturnType = (HttpReturnType) params[3];
+			payload = (String) params[1];
+			headers = (Map<String, String>) params[2];
+			callback = (NetworkResponse) params[3];
+			_ReturnType = (HttpReturnType) params[4];
 			
 			try {
-
-				HttpClient client = new DefaultHttpClient();
-				final HttpGet get = new HttpGet(url);
-
-				get.setHeader("User-Agent", userAgent);
+				HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+				conn.setRequestProperty("User-Agent", userAgent);
+				conn.setRequestMethod(verb.name());
 				if (headers != null) for (String name : headers.keySet()) {
-					get.setHeader(name, headers.get(name));
+					conn.setRequestProperty(name, headers.get(name));
 				}
-				callback.setOnCancelledListener(new OnCancelledListener() {
-					
-					@Override
-					public void onCancel() {
-						get.abort();
-					}
-				});
 
-				HttpResponse getResponse = client.execute(get);
-				try {
-					res = getResponse.getStatusLine().getStatusCode();
-				} catch (Throwable t) {}
-				Header[] responseHeaders = getResponse.getAllHeaders();
+				if (verb._HasPayload) {
+					conn.setDoOutput(true);
+					OutputStream os = conn.getOutputStream();
+					os.write(payload.getBytes());
+					os.close();
+				}
 
+				res = conn.getResponseCode();
+				Map<String, List<String>> reqProps = conn.getHeaderFields();
 				responseHeaderMap = new HashMap<>();
-				for (Header h : responseHeaders) {
-					responseHeaderMap.put(h.getName(), h.getValue());
+				Set<String> reqKeys = reqProps.keySet();
+				for (String reqKey : reqKeys) {
+					responseHeaderMap.put(reqKey, reqProps.get(reqKey).get(0));
 				}
-
-				result = getResult(_ReturnType, getResponse.getEntity());
+				result = getResult(_ReturnType, conn);
 				return true;
 
 			} catch (Exception e) {
@@ -121,140 +171,25 @@ public class AndroidNetwork implements NetworkDelegateClient {
 				callback.callOnFail(code);
 			}
 		}
-
-	}
-
-	private class PostPutAsyncTask extends AsyncTask<Object, Void, Boolean> {
-		String url;
-		String toPost;
-		Map<String, String> headers;
-		NetworkResponse callback;
-
-		int res = 200;
-		String result;
-		Map<String, String> responseHeaderMap;
-
-		String code;
-		private HttpVerb _Verb;
-		private HttpReturnType _ReturnType;
-		
-		public PostPutAsyncTask(HttpVerb verb) {
-			this._Verb = verb;
-		}
-
-		@SuppressWarnings("unchecked")
-		@Override
-		protected Boolean doInBackground(Object... params) {
-			url = (String) params[0];
-			toPost = (String) params[1];
-			headers = (Map<String, String>) params[2];
-			callback = (NetworkResponse) params[3];
-			_ReturnType = (HttpReturnType) params[4];
-			
-			try {
-				HttpClient client = new DefaultHttpClient();
-
-				HttpEntityEnclosingRequestBase base;
-				if (_Verb == HttpVerb.POST) {
-					base = new HttpPost(url);
-				} else if (_Verb == HttpVerb.PUT) {
-					base = new HttpPut(url);
-				} else {
-					throw new IllegalStateException("verb is " + _Verb
-							+ ", can only be put or post");
-				}
-				if (headers != null) for (String name : headers.keySet()) {
-					base.setHeader(name, headers.get(name));
-				}
-
-				base.setEntity(new StringEntity(toPost, HTTP.UTF_8));
-
-				HttpResponse postResponse = client.execute(base);
-				try {
-					res = postResponse.getStatusLine().getStatusCode();
-				} catch (Throwable t) {}
-
-				Header[] responseHeaders = postResponse.getAllHeaders();
-
-				responseHeaderMap = new HashMap<>();
-				for (Header h : responseHeaders) {
-					responseHeaderMap.put(h.getName(), h.getValue());
-				}
-
-				result = getResult(_ReturnType, postResponse.getEntity());
-
-				return true;
-
-			} catch (Exception e) {
-				code = e.getLocalizedMessage();
-				return false;
-			}
-
-		}
-
-		@Override
-		protected void onPostExecute(Boolean success) {
-			super.onPostExecute(success);
-
-            if (success && res >= 200 && res <= 299) {
-				callback.callOnSuccess(res, result, responseHeaderMap);
-			} else {
-				callback.callOnFail(code);
-			}
-		}
-
 	}
 
 	@Override
 	public void doHttp(HttpVerb verb, String url, String payload, Map<String, String> headers,
 			NetworkResponse callback) {
-		switch (verb) {
-			case GET:
-				new GetAsyncTask().execute(url, headers, callback, HttpReturnType.Plain);
-				break;
-			case POST:
-			case PUT:
-				new PostPutAsyncTask(verb).execute(url, payload, headers, callback, HttpReturnType.Plain);
-				break;
-
-			default:
-				throw new IllegalArgumentException(verb + " unsupported");
-		}
+		new NetAsyncTask(verb).execute(url, payload, headers, callback, HttpReturnType.Plain);
 	}
 
 	@Override
 	public void doHttpWithBase64Return(HttpVerb verb, String url,
 			String payload, Map<String, String> headers,
 			NetworkResponse callback) {
-		switch (verb) {
-			case GET:
-				new GetAsyncTask().execute(url, headers, callback, HttpReturnType.Base64);
-				break;
-			case POST:
-			case PUT:
-				new PostPutAsyncTask(verb).execute(url, payload, headers, callback, HttpReturnType.Base64);
-				break;
-	
-			default:
-				throw new IllegalArgumentException(verb + " unsupported");
-		}
+		new NetAsyncTask(verb).execute(url, payload, headers, callback, HttpReturnType.Plain);
 	}
 
 	@Override
 	public void doHttpWithTokenReturn(HttpVerb verb, String url,
 									   String payload, Map<String, String> headers,
 									   NetworkResponse callback) {
-		switch (verb) {
-			case GET:
-				new GetAsyncTask().execute(url, headers, callback, HttpReturnType.Token);
-				break;
-			case POST:
-			case PUT:
-				new PostPutAsyncTask(verb).execute(url, payload, headers, callback, HttpReturnType.Token);
-				break;
-
-			default:
-				throw new IllegalArgumentException(verb + " unsupported");
-		}
+		new NetAsyncTask(verb).execute(url, payload, headers, callback, HttpReturnType.Plain);
 	}
 }
